16d7a103d8b3ff54b1126ebdeef597d97468b283
Nahaivska13 <Nahaivska13.daryna.nahaivska2017@gmail.com>
2024-11-10 16:10:19 +0000

    Add unit test and CI configuration

---
+++ build/CMakeFiles/test_program.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_program.dir/euler.cpp.o"
+  "CMakeFiles/test_program.dir/euler.cpp.o.d"
+  "CMakeFiles/test_program.dir/main_test.cpp.o"
+  "CMakeFiles/test_program.dir/main_test.cpp.o.d"
+  "test_program"
+  "test_program.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ test_euler.cpp
@@ -0,0 +1,22 @@
+#include <cassert>
+#include <cmath>
+#include <iostream>
+#include "euler.h"
+
+void testSecSeries() {
+    Euler euler;
+    int n = 5;
+    double x = 0.5;
+    double result = euler.secSeries(n, x);
+    std::cout << "Result: " << result << std::endl;
+    double expected = 1.139493;
+    double epsilon = 1e-6;
+    assert(fabs(result - expected) < epsilon);
+}
+
+int main() {
+    testSecSeries();
+    std::cout << "Test passed!" << std::endl;
+    return 0;
+}
+
