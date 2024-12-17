fc3b5e9ff1d6837906932c290f21a5683cb7ef3b
Nahaivska13 <Nahaivska13.daryna.nahaivska2017@gmail.com>
2024-12-17 00:03:21 +0000

    Edit autodeploy and scriptB

---
+++ scriptB.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+URL="http://<nginx-ip>/compute"
+
+# Функція для відправки HTTP-запиту
+send_request() {
+    # Надсилаємо запит і вимірюємо час відповіді
+    start=$(date +%s%3N)
+    curl -s $URL > /dev/null
+    end=$(date +%s%3N)
+
+    # Обчислюємо час відповіді
+    response_time=$((end - start))
+    echo "Response time: ${response_time} ms"
+}
+
+# Нескінченний цикл
+while true; do
+    # Випадковий час сну між 5 і 10 секундами
+    sleep_interval=$((RANDOM % 6 + 5))
+
+    # Виводимо інформацію про час сну
+    echo "Sleeping for ${sleep_interval} seconds..."
+
+    # Чекаємо випадковий час
+    sleep $sleep_interval
+
+    # Викликаємо функцію для відправки запиту
+    send_request
+done
+
